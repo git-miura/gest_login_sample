96a26bea13b64c895f0b5779f9a7ae7d6a8713cb
三浦 <miura.mai910@gmail.com>
2021-06-08 22:49:27 +0900

    画像ファイル投稿機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :comment)
+    params.require(:post).permit(:title, :comment, :image)
   end
 end
