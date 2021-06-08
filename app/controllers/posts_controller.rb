fe7603ee2165042250c543007d9d91dd0111d0c3
三浦 <miura.mai910@gmail.com>
2021-06-08 23:49:16 +0900

    投稿・削除機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,29 @@ class PostsController < ApplicationController
 
   def create
     current_user.posts.create!(post_params)
+    redirect_to root_path
+
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy!
+    redirect_to root_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to post
+  end
 
   private
 
