518466f8dc22105b629fdc37c42789e0611e1096
三浦 <miura.mai910@gmail.com>
2021-06-08 23:41:13 +0900

    詳細表示機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: :asc)
   end
 
   def new
@@ -12,6 +12,10 @@ class PostsController < ApplicationController
 
   def create
     current_user.posts.create!(post_params)
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
 
